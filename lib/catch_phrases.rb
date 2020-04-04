855b566522c742b912d8cc28c8c09b92666f405d
balanced-widget-4002 <jessikim807@gmail.com>
2020-04-04 18:49:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,9 @@
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts $status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
 end
